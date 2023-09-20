cfa9981fc26ff6d6d03d054722151818bfdb9759
DannySolomon <85731129+DannySolomon@users.noreply.github.com>
2023-09-20 15:49:15 +0530

    Update question_controller.js
    Return response with question id when creating a question

--- controllers/question_controller.js
+++ controllers/question_controller.js
@@ -30,7 +30,7 @@ module.exports.questionCreate = async (req, res) => {
     return res.send("Couldnt create question ", err);
   });
 
-  return res.send("Question created successfully");
+  return res.send(`Question created successfully ${newquestion.id}`);
 };
 
 //view a particular question
